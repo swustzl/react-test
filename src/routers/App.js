a039f40839309ecf0079bfc2781699858d656b2e
zl.zhang <swust_zl@163.com>
2018-11-19 17:01:13 +0800

    1119

---
+++ src/routers/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import Home from './home';
+
+class App extends Component {
+  render() {
+    return (
+        <BrowserRouter>
+          <div>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/about/">About</Link>
+                </li>
+              </ul>
+            </nav>
+
+            <Route path="/" exact component={Home} />
+            {/*<Route path="/about/" component={About} />*/}
+          </div>
+        </BrowserRouter>
+    );
+  }
+}
+
+export default App;
